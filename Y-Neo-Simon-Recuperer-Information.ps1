02e1896a61157f5d9018c3633d8ec5134cfc846a
jamer-coding <pb62kjx@eduvaud.ch>
2025-02-16 17:05:11 +0100

    Version 1.0.0
    This version includes everything that works locally.

--- Y-Neo-Simon-Recuperer-Information.ps1
+++ Y-Neo-Simon-Recuperer-Information.ps1
@@ -15,13 +15,13 @@
  	Auteur: Simon et Néo
  	Raisons: Continuation du script
 
-    Date  : - 03.02.2025
- 	Auteur: - Simon et Néo
- 	Raisons: - Version 1.0.0 du script (marche localement)
+    Date  : 03.02.2025
+ 	Auteur: Simon et Néo
+ 	Raisons: [VERSION 0.9.0] du script (marche localement, manque les applications & multiples disques)
 
-    Date  : -
- 	Auteur: -
- 	Raisons: -
+    Date  : 16.02.2025
+ 	Auteur: Simon et Néo
+ 	Raisons: [VERSION 1.0.0] Correction et optimisation des boucles, ajout des programmes installés dans le fichier log et l'option d'avoir un fichier log ou non
 
     Date  : -
  	Auteur: -
@@ -42,13 +42,13 @@
 .DESCRIPTION
     Description plus détaillée du script, avec les actions et les tests effectuées ainsi que les résultats possibles
 
-.PARAMETER CreateNewFile
-    Description du premier paramètre avec les limites et contraintes
-
-.PARAMETER Param2
+.PARAMETER LogFilePath
+    Le paramètre LogFilePath spécifie le chemin où le fichier journal doit être sauvegardé.
+    Il doit s'agir d'un chemin de fichier `.log` valide.
+.PARAMETER DistantComputerName
     Description du deuxième paramètre avec les limites et contraintes
 
-.PARAMETER Param3
+.PARAMETER DistantComputerIP
     Description du troisième paramètre avec les limites et contraintes
 
 .OUTPUTS
@@ -73,83 +73,130 @@
    Un paramètre peut être initialisé : $Param2="Toto"
    Un paramètre peut être obligatoire : [Parameter(Mandatory=$True][string]$Param3
 #>
-# La définition des paramètres se trouve juste après l'en-tête et un commentaire sur le.s paramètre.s est obligatoire 
-param($distantComputerName)
+# La définition des paramètres se trouve juste après l'en-tête et un commentaire sur le.s paramètre.s est obligatoire
+param($LogFilePath, $DistantComputerName, $DistantComputerIP)
 
 ###################################################################################################################
-# Zone de définition des variables et fonctions, avec exemples
+
 $titre_date = Get-Date -Format "yyyy.MM.dd hh:mm:ss"                                                # Date utilisé dans l'en-tête
 $titre_top = "╔═══════════════════════════════════════════════════════════════════════════════╗"    # En-tête du script (s'affiche dans powershell)
 $titre_hea = "║                                 SYSINFO LOGGER                                ║"    # En-tête du script (s'affiche dans powershell)
 $titre_mid = "╟═══════════════════════════════════════════════════════════════════════════════╣"    # En-tête du script (s'affiche dans powershell)
 $titre_bod = "║ Collecte fait le $titre_date                                          ║"            # En-tête du script (s'affiche dans powershell)
 $titre_end = "╚═══════════════════════════════════════════════════════════════════════════════╝"    # En-tête du script (s'affiche dans powershell)
+
+$date_log = Get-Date -Format "yyyy-MM-dd hh:mm"                                                     # Date de journalisation
+$titre_log                                                                                          # Titre du fichier log
+
+$Path                                                                                               # Chemin du fichier log
+
 $adress                                                                                             # Addresse IP de l'ordinateur
-$computerInfo                                                                                       # 
-$systemInfo
-$date_log = Get-Date -Format "yyyy-MM-dd hh:mm"
-$titre_log
-$processor
-$gpu
-$disk
-$diskSize
-$diskRemainingSize
-$ram
-$ramUsed
-$apps
-$timezone = Get-TimeZone
 
-# Fonction utilisé pour écrire les objets d'une table dans le fichier .log
-function outputTableItems([Parameter(Mandatory = $True)] [table]$table)
-{
-    # Initialisation des variables
-    $currentCount = 0:                  # Compteur pour la boucle for each
-    $tableSize = $table.Length - 1      # Taille de la table (moins un pour avoir 0)
+$computerInfo                                                                                       # Infos sur l'ordinateur
+$systemInfo                                                                                         # Système d'exploitation
 
-    # A MODIFIER                                                            
+$processor                                                                                          # Infos processeur
+$gpu                                                                                                # Infos carte graphique
 
-    # Boucle for each pour écrire toutes les applications, séparé d'une virgule
-    foreach ($item in $table) {
-        Add-Content .\log\sysloginfo.log -Value $item -NoNewline
+$disks                                                                                              # Infos disques de stockage
 
-        # Vérifie que $application n'est pas la dernière instance de $apps
-        if (!($table[$tableSize] -eq $item)) {
-            Add-Content .\log\sysloginfo.log -Value ", " -NoNewline
+$ram                                                                                                # Infos RAM totale
+$ramUsed                                                                                            # Infos RAM utilisée                                                                                            #
+
+$packages                                                                                           #Paquets installés sur le système Les packets logiciels installés sur l'ordinateur
+
+$timezone = Get-TimeZone                                                                            # Fuseau Horaire
+
+# Fonction utilisé pour écrire les applications dans le fichier .log
+function Write-Apps([Parameter(Mandatory = $True)] $packetTable) {
+    $appsTable = @()                                # Créer une nouvelle table pour storer les applications plus tard
+
+    # Boucle for each pour mettre toutes les applications dans une table séparée
+    foreach ($packet in $packetTable) {
+
+        # Vérifie que le packet est bel et bien un programme
+        if ($packet.ProviderName -eq "Programs") {
+            $appsTable += $packet
         }
+    }
+
+    # Boucle for each pour écrire toute les applications dans le fichier log
+    foreach ($app in $appsTable) {
+        # Créer la ligne qui va s'affiche pour l'application actuelle
+        $appLine = $app.Name + " Version " + $app.Version
+
+        # Afficher la ligne
+        Add-Content $Path -Value $appLine -NoNewline
+
+        # Vérifie que $app n'est pas la dernière instance de $apps
+        if (!($appsTable[-1] -eq $app)) {
+            Add-Content $Path -Value ", " -NoNewline
+        }
+        # Si c'est la dernière application
+        else {
+            Add-Content $Path -Value "`n" -NoNewline
+        }
+
     }
 }
 
 ###################################################################################################################
-# Zone de tests comme les paramètres renseignés ou les droits administrateurs
 
-# Ajout du fichier log en UTF-8 s'il n'existe pas
-if (!(Test-Path -Path ".\log\sysloginfo.log")) {
-    New-Item -ItemType File -Path ".\log\sysloginfo.log"
-    Set-Content -Path ".\log\sysloginfo.log" -Encoding utf8 -Value $null
+# Verifier que $LogPath n'est pas vide
+if (!($null -eq $LogFilePath)) {
+    # Verifier que $LogPath est un chemin valide et qui mène à un fichier log
+    if ($LogFilePath -match '\.log$' -and (Test-Path -Path (Split-Path -Parent $LogFilePath))) {
+        # Ajouter le chemin à la variable $Path
+        $Path = $LogFilePath
+    }
+    # Le chemin n'existe pas
+    else {
+        # Efface la console
+        Clear-Host
+
+        # Montre l'aide du paramètre LogFilePath
+        (Get-Help $MyInvocation.MyCommand.Path).parameters.parameter |
+        Where-Object { $_.name -eq "LogFilePath" } |
+        Select-Object -ExpandProperty description | ForEach-Object { $_.Text } |
+        Write-Host -ForegroundColor Yellow
+
+        # Ajoute un retour à la ligne
+        Write-Host $null
+
+        # Lance une erreur
+        throw [System.ArgumentException]::new("La valeur donnée au paramètre LogPath n'est pas valide. Veuillez réessayer avec l'aide montrée en dessus.")
+
+        # Ferme le programme
+        exit
+    }
+}
+# $LogPath est vide
+else {
+    $Path = $null
 }
 
 ###################################################################################################################
-# Corps du script
 
 # Initialisation de variables
+$titre_log = $date_log + " - " + $systemInfo.Name + "/" + $adresse.IPAddress + " - "
+
 $adress = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress
+
 $computerInfo = Get-ComputerInfo
 $systemInfo = Get-CimInstance -ClassName Win32_ComputerSystem
-$titre_log = $date_log + " - " + $systemInfo.Name + "/" + $adresse.IPAddress + " - "
 
 $processor = (Get-CimInstance -ClassName CIM_Processor).Name
 $gpu = (Get-CimInstance -ClassName CIM_VideoController).Name
-$ram = [Math]::Round($computerInfo.CsTotalPhysicalMemory/1GB, 2)
-$ramUsed = [Math]::Round($computerInfo.CsTotalPhysicalMemory/1GB - $computerInfo.OsFreePhysicalMemory/1MB, 2)
-$disk = Get-Volume -DriveLetter C
-$diskSize = [Math]::Round($disk.Size/1GB, 2)
-$diskRemainingSize = [Math]::Round(($disk.Size - $disk.SizeRemaining)/1GB, 2)
 
-$apps = Get-CimInstance -ClassName Win32_InstalledStoreProgram |Format-Table -AutoSize
+$ram = [Math]::Round($computerInfo.CsTotalPhysicalMemory / 1GB, 2)
+$ramUsed = [Math]::Round($computerInfo.CsTotalPhysicalMemory / 1GB - $computerInfo.OsFreePhysicalMemory / 1MB, 2)
+
+$disks = Get-Volume
+
+$packages = Get-Package
 
 # Effacer les entrées
 Clear-Host
-
 # Affichage de l'en-tête dans le terminal
 Write-Host $titre_top
 Write-Host $titre_hea
@@ -170,20 +217,70 @@ Write-Host ""
 Write-Host "┌ HARDWARE"
 Write-Host "| CPU:`t`t" $processor
 Write-Host "| GPU 0:`t" $gpu
-Write-Host "| Disque C:`t" $diskRemainingSize "/" $diskSize "GB"
+
+# Afficher les disques
+
+#boucle foreach pour tout les disques
+foreach ($disk in $disks) {
+    # Prendre la lettre du volume
+    $diskLetter = $disk.DriveLetter
+
+    # Prendre le type du volume
+    $diskType = $disk.DriveType
+
+    # Si la lettre n'est pas null et que le type de volume n'est pas CD-ROM
+    if (!($null -eq $diskLetter -or "CD-ROM" -eq $diskType)) {
+        # Prendre toute les informations utiles
+        $diskSize = [Math]::Round($disk.Size / 1GB, 2)
+        $diskRemainingSize = [Math]::Round(($disk.Size - $disk.SizeRemaining) / 1GB, 2)
+
+        # Afficher le disque
+        Write-Host "| Disque"$diskLetter":`t" $diskRemainingSize "/" $diskSize "GB"
+    }
+}
+
+# Afficher la ram
 Write-Host "└ RAM:`t`t" $ramUsed "/" $ram "GB"
 Write-Host ""
 
-# Affichage des informations dans le fichier sysloginfo.log
-$titre_log + "OS: " + $computerInfo.OsName +
-             " - Version: " + $computerInfo.OsVersion + " Build " + $computerInfo.OsBuildNumber +
-             " - Utilisation de l'espace disque C: " + $diskRemainingSize + " / " + $diskSize + " GB"  +
-             " - RAM: " + $ramUsed + " / " + $ram + " GB" | Add-Content .\log\sysloginfo.log
+# Verifier que $Path n'est pas null
+if (!($null -eq $Path)) {
+    # Affichage des informations dans le fichier sysloginfo.log
+    $titre_log + "OS: " + $computerInfo.OsName +
+    " - Version: " + $computerInfo.OsVersion + " Build " + $computerInfo.OsBuildNumber | Add-Content .\log\sysloginfo.log -NoNewline
 
-$titre_log + "Programmes installés : " | Add-Content .\log\sysloginfo.log -NoNewline
+    # Afficher les disques
 
-# Appel de la fonction outputTableItems
-outputTableItems($apps)
+    #boucle foreach pour tout les disques
+    #boucle foreach pour tout les disques
+    foreach ($disk in $disks) {
+        # Prendre la lettre du volume
+        $diskLetter = $disk.DriveLetter
 
+        # Prendre le type du volume
+        $diskType = $disk.DriveType
 
-$titre_log + "`nFuseau Horaire : " + $timezone + "`n" | Add-Content .\log\sysloginfo.log
+        # Si la lettre n'est pas null et que le type de volume n'est pas CD-ROM
+        if (!($null -eq $diskLetter -or "CD-ROM" -eq $diskType)) {
+            # Prendre toute les informations utiles
+            $diskSize = [Math]::Round($disk.Size / 1GB, 2)
+            $diskRemainingSize = [Math]::Round(($disk.Size - $disk.SizeRemaining) / 1GB, 2)
+
+            # Afficher le disque dans le fichier log
+            " - Utilisation de l'espace disque " + $diskLetter + ": " +
+            $diskRemainingSize + " / " + $diskSize + " GB" | Add-Content $Path -NoNewline
+        }
+    }
+
+    # Afficher le reste
+    " - RAM: " + $ramUsed + " / " + $ram + " GB" | Add-Content $Path
+
+    # Afficher les programmes installés
+    $titre_log + "Programmes installés : " | Add-Content $Path -NoNewline
+
+    # Appel de la fonction Write-Apps
+    Write-Apps($packages)
+
+    # Afficher le fuseau horraire
+    $titre_log + "Fuseau Horaire : " + $timezone + "`n" | Add-Content $Path
+}
